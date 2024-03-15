6c30e8e02ec1134a05665fa3337c5353693358b4
Sandeepkumar2973 <106466194+Sandeepkumar2973@users.noreply.github.com>
2024-03-15 11:48:58 +0530

    sk

--- veeaarsofttech/src/Components/Loder/Loder.js
+++ veeaarsofttech/src/Components/Loder/Loder.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useLoader } from "./LoderContecxt.js";
 import { Vortex } from "react-loader-spinner";
 import "./Loader.css";
-import HashLoader from 'react-spinners/HashLoader';
+import HashLoader from "react-spinners/HashLoader";
 
 const Loader = () => {
   const { loading } = useLoader();
 
   return loading ? (
     <div className="container loder">
-      <HashLoader color="#36d7b7"  className="loder"/>
+      <HashLoader color="#36d7b7" className="loder" />
     </div>
   ) : null;
 };
